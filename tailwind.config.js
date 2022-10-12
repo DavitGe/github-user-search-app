c1d704dc243ce4be8a993783a17c43332b29220d
DavitGe <davitge303@gmail.com>
2022-10-12 22:27:42 +0400

    Setting defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,23 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      secondary: "#0079FF",
+      main: "#FEFEFE",
+      bgColor: "#F6F8FF",
+      text: "#4B6A9B",
+      title: "#2B3442",
+      date: "#697C9A",
+      dMain: "#1E2A47",
+      dBg: "#141D2F",
+      dText: "#FFFFFF",
+    },
+    fontSize: {
+      sm: ["13px", "20px"],
+      base: ["16px", "24px"],
+      lg: ["22px", "33px"],
+      xl: ["26px", "38px"],
+    },
   },
   plugins: [],
 };
